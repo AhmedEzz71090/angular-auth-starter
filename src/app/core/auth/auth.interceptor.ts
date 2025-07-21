7fbd02a89111fc0f51da0f78e7eb0c78b792f218
ahmed-ezz <a.ezz@afaqy.com>
2025-07-22 00:18:41 +0300

    Install firebase, connect to it, and apply firebase authentication

--- src/app/core/auth/auth.interceptor.ts
+++ src/app/core/auth/auth.interceptor.ts
@@ -1,16 +1,38 @@
+//Normal
+// import { HttpInterceptorFn } from '@angular/common/http';
+// import { inject } from '@angular/core';
+// import { AuthService } from './auth.service';
+//
+// export const authInterceptor: HttpInterceptorFn = (req, next) => {
+//   const authService = inject(AuthService);
+//   const token = authService.getToken();
+//
+//   if (token) {
+//     req = req.clone({
+//       setHeaders: { Authorization: `Bearer ${token}` }
+//     });
+//   }
+//
+//   return next(req);
+// };
+
+//Firebase
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from './auth.service';
+import { from, switchMap } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
-  const token = authService.getToken();
 
-  if (token) {
-    req = req.clone({
-      setHeaders: { Authorization: `Bearer ${token}` }
-    });
-  }
-
-  return next(req);
+  return from(authService.getIdToken()).pipe(
+    switchMap((token) => {
+      if (token) {
+        req = req.clone({
+          setHeaders: { Authorization: `Bearer ${token}` }
+        });
+      }
+      return next(req);
+    })
+  );
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:3000/api'
+  // apiBaseUrl: 'http://localhost:3000/api',
+  firebase: {
+    apiKey: "AIzaSyAqfni9lW5m3zkQbvSr0zBaJ0FtiombkBQ",
+    authDomain: "angular-auth-starter.firebaseapp.com",
+    projectId: "angular-auth-starter",
+    storageBucket: "angular-auth-starter.firebasestorage.app",
+    messagingSenderId: "161239330886",
+    appId: "1:161239330886:web:75e9294eefe47b6e965a47",
+    measurementId: "G-M3NX8FE9ZS"
+  }
 };
